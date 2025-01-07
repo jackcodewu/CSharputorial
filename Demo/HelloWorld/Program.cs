19c120cfa8cb4aec2ca6f271f86a395199b25914
arlo <cocoboywu@gmail.com>
2025-01-07 15:53:37 +0800

    增加繁体PPT

--- Demo/HelloWorld/Program.cs
+++ Demo/HelloWorld/Program.cs
@@ -7,6 +7,14 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            if (args?.Any() == true)
+            {
+                foreach (var item in args)
+                {
+                    Console.WriteLine($"传入的参数是：{item}");
+                }
+            }
+
             Console.ReadLine();
 
         }
